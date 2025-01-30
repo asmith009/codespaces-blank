5527ab40078aac1b7b7614f1720753c252d7c70d
asmith009 <158201326+asmith009@users.noreply.github.com>
2025-01-30 13:46:45 +0000

    first commit

---
+++ problemSolving.py
@@ -0,0 +1,29 @@
+#January 30, 2025
+
+# ROCK, PAPER, SCISSOR GAME APP
+# You are working on a game concept with several 
+# other engineers and you need to provide them with a
+#  simple proof of concepts. 
+# For now, you have suggested that your 
+# game work on a RPS (rock, paper, scissors) loop. 
+# When a user plays, rock, paper, or scissors, 
+# the computer will randomly select one of the 3 
+# options as well. Your program will then determine 
+# who won, based on the values. The game should only 
+# allow the player to play 3 hands, and inform the 
+# player if the won or lost (best 2 out of 3 format.) 
+
+#  1. What is the problem asking you to do?
+# The problem is asking to make a function using a list to randomly select from by the user and the computer to decipher who won or lost.
+# 2. What are some keywords and phrases ? 
+# Some key words and phrases are, loop, rock, paper, and scissors, randomly select out of three options, and to determine who won based on their values.
+# 3. Is there input data that the question provides ?
+# There will be 2 or more input datas in this function due to this game running for 3 rounds, and determining who won by the second.
+# 4. What is the desired output ?
+# The desired output is which of the randomly selected options won between the user and the computer, plus the ammount of rounds.
+# 5. Pseudocode Steps & Additional Notes 
+# we will need to make a list for the user and the computer to choose from.
+# we will also have to use greater than or less than to verify the values of each choise.
+# Then we will need to make a loop occurung 2 or more times.
+# Then allow the function to decide who won.
+# Solution
