7341b73f9b18c32289a3e5ed5a0f74312a278660
asmith009 <158201326+asmith009@users.noreply.github.com>
2025-02-03 13:33:56 +0000

    asyncReveiwJanuary24

---
+++ asyncReviewJan24.py
@@ -0,0 +1,28 @@
+# January 24th, 2025
+
+#Boys Latin has hired you to develop a grade checker program for students. 
+#Your program should be able to do the following; allow a student to enter a numerical grade, 
+#then return the appropriate letter grade. For example; if a student enters the number 80, the program should give back the letter grade "B". 
+#Your program should then repeat by asking the user to enter another numerical grade. 
+#Each time a user enters another numerical grade, it should perform some calculation to average out the grades and show the new letter grade, 
+#For example: if the user enters the number 70, and the 100, they're average should be an 85, which is a B.
+
+#1. In a complete sentence, what is the problem asking you to do ?
+#The problem is asking for me to make a program that allows a stusent to inpoout their grade numerous times, then give the average grade.
+
+#2. Were there any parts of the question that don't make sense, if so write them down in complete sentences.
+# This part of the promt doesnt necessarily make sense, though im un able to understand how I would print the average grade.
+
+#3. Please write down the keywords and phrases that will help you solve this problem ?
+#   1. enter a number.    2. repeat.    3. average.    4. calculations.    5. return appropiate lette grade(output).
+
+#4. Does the problem provide you with any input data? Does it tell you what type of data your program needs to take in ?
+# The program is asking the user to inoput a number numerous times, in which the program will calculate the average amongst those grade.
+
+#5. Does the problem provide you with desired output data? Does it tell you that your program needs to give somthing back to the user ?
+# The proogram will produce the avergage letter grade.
+
+#6. Using Pseudocode (fake code), write down Step by step how you think you can solve this problem. Remember, this does not have to be code.
+# first i would give the program a name.
+# then i would insert various inputs, for the user to input their grade.
+# then i would use arithmatic coding for the computer to read, so it gives the avergae letter grade.
